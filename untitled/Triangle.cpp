f9a110fee1e546166d367e96fb87f83ee568c662
sumboid <ilya.sumb@gmail.com>
2012-04-18 10:22:17 +0700

    sync

--- untitled/Triangle.cpp
+++ untitled/Triangle.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "Triangle.h"
 
 Triangle::Triangle(View* _view, const QImage& _image):
@@ -23,12 +24,52 @@ void Triangle::setScale(const double& _scale)
     scale = _scale;
 }
 
-void Triangle::drawLine(const Point x, const Point y)
+std::vector<Point> Triangle::drawLine(const Point _x, const Point _y)
 {
+    Point x = _x;
+    Point y = _y;
+    std::vector<Point> line;
+    const int deltaX = abs(x.first - y.first);
+    const int deltaY = abs(y.second - x.second);
+    const int signX = x.first < y.first ? 1 : -1;
+    const int signY = x.second < y.second ? 1 : -1;
+
+    int error = deltaX - deltaY;
+    setPixel(y.first, y.second);
+    while(x.first != y.first || x.second != y.second)
+    {
+        line.insert(Point(x.first, x.second));
+        const int error2 = error * 2;
+        if(error2 > -deltaY)
+        {
+            error -= deltaY;
+            x.first += signX;
+        }
+        if(error2 < deltaX)
+        {
+            error += deltaX;
+            x.second += signY;
+        }
+    }
+
+    return line;
 }
+
 
 void Triangle::draw(const Point x, const double angle)
 {
+    /* Hurdcoar
+     *
+     *  [a]
+     *   |\
+     *   | \
+     *   |  \
+     *   |   \
+     *  [c]--[b]
+     *
+     */
+
+    Point a, b, c = x;
 
     return;
 }
--- untitled/Triangle.h
+++ untitled/Triangle.h
@@ -2,6 +2,7 @@
 #define TRIANGLE_H
 
 #include <utility>
+#include <vector>
 #include <QImage>
 
 #include "View.h"
@@ -17,14 +18,11 @@ public:
     void setScale(const double&);
     void draw(const Point x, const double angle);
 private:
-    void drawLine(const Point x, const Point y);
+    a.first = scale * (imagePoints[1].first - imagePoints[0].first) + c.first;
+    a.second = scale * (imagePoints[1].second - imagePoints[0].second) + c.second;
+    std::vector<Point> getLine(const Point x, const Point y);
 
-    struct Coordinates
-    {
-        Point x[3];
-    };
-
-    Coordinates imageCoordinates;
+    Point imagePoints[3]; /* [c], [a], [b] */
     double scale;
 
     View* view;
