b8792377144333a64d3a01408810bfd0c66088e1
yobushka <ilya.sumb@gmail.com>
2012-05-15 17:20:09 +0700

    rotate fix

--- FIT9201SUMBATYANTS_Knot/Knot.cpp
+++ FIT9201SUMBATYANTS_Knot/Knot.cpp
@@ -29,12 +29,12 @@ void Knot::generatePoints()
         tmp.subtract(ip0);
         tmp.normalize();
         Vector wp1(wp0);
-        wp1.subtract(wp0);
+        wp1.subtract(tmp);
         Vector wp2(wp0);
-        wp2.add(wp0);
+        wp2.add(tmp);
 
-        workPoints.push_back(Vector(wp0));
         workPoints.push_back(Vector(wp1));
+        workPoints.push_back(Vector(wp0));
         workPoints.push_back(Vector(wp2));
     }
 }
@@ -43,7 +43,7 @@ void Knot::generatePoints()
 void Knot::draw(View* view, const Camera* camera, unsigned int color)
 {
     const int n = initPoints.size();
-    for(int i = 0; i < n; i+=3)
+    for(int i = 1; i < n; i+=3)
     {
         Vector bPoints[4];
         for(int k = 0; k < 4; k++)
